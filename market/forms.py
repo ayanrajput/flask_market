1e6bcd17261f99002767b73334d0bd4696bda4cf
Ayan <ayanrajput1000@gmail.com>
2021-06-24 21:16:35 +0530

    Implemented Uniqueness Check in registration form

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length,EqualTo,Email
+from wtforms.validators import Length,EqualTo,Email,DataRequired,ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
-    username=StringField(label="User Name:",validators=[Length(min=2,max=30)])
-    email_address=StringField(label="Email:",validators=[Email()])
-    password1=PasswordField(label="Password:",validators=[Length(min=6)])
-    password2=PasswordField(label="Confirm Password:",validators=[EqualTo("password1")])
+    def validate_username(self,username_to_check):
+        user=User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please try a different username")
+    def validate_email_address(self,email_address_to_check):
+        email_address=User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError("Email already exists! Please try a different email")
+
+
+    username=StringField(label="User Name:",validators=[Length(min=2,max=30),DataRequired()])
+    email_address=StringField(label="Email:",validators=[Email(),DataRequired()])
+    password1=PasswordField(label="Password:",validators=[Length(min=6),DataRequired()])
+    password2=PasswordField(label="Confirm Password:",validators=[EqualTo("password1"),DataRequired()])
     submit=SubmitField(label="Create Account")

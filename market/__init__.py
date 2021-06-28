16cf636e65dc1208b2559c4b2c8e3e4dfddae7bc
Ayan <ayanrajput1000@gmail.com>
2021-06-28 20:27:05 +0530

    implemented login page and login

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///market.db"
 app.config["SECRET_KEY"]='ca7c566aef335679b5b41087'
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,9 @@
 from market import app
 from flask import render_template,redirect,url_for,flash
 from market.models import Item,User
-from market.forms import RegisterForm
+from market.forms import RegisterForm,LoginForm
 from market import db
+from flask_login import login_user
 @app.route('/')
 @app.route("/home")
 def home_page():
@@ -25,3 +26,20 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f"There was an error when creating user: {err_msg}",category="danger")
     return render_template("register.html",form=form)
+
+
+
+
+@app.route("/login",methods=["GET","POST"])
+def login_page():
+    form=LoginForm()
+    if form.validate_on_submit():
+        attempted_user=User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f"Succesfully Logged in as: {attempted_user.username}",category="success")
+            return redirect(url_for("market_page"))
+        else:
+            flash("Username and password dont match! Please try again",category="danger")
+
+    return render_template("login.html",form=form)
